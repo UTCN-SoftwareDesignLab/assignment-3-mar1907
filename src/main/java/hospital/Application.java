30dcc2a18e144346a4cf2bf0ade9eb45225c2f47
mar1907 <marius.supuran@yahoo.com>
2018-04-26 16:42:56 +0300

    Login (Spring Security) created

--- src/main/java/hospital/Application.java
+++ src/main/java/hospital/Application.java
@@ -1,4 +1,24 @@
 package hospital;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+/**
+ * Created by Catalysts on 8/9/2015.
+ */
+
+@EnableAutoConfiguration
+@ComponentScan
+@SpringBootApplication
+@EnableJpaRepositories(basePackages = {"hospital.repository"})
+@PropertySource(value = "classpath:application.properties")
 public class Application {
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Application.class, args);
+    }
 }
